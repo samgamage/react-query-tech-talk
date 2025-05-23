11a9597160dbe8dc8c723e46a4a8bee2ec753ed8
samgamage <sam.gamage@clasp.com>
2025-05-23 09:00:47 -0400

    Fix rebase

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 // Create a client
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <QueryClientProvider client={queryClient}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </QueryClientProvider>
   );
 }
